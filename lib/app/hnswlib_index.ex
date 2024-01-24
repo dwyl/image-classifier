67476f2339558edbb16a06ef55755dd9e3655dbf
Neven DREAN <nevendrean@yahoo.fr>
2024-01-23 22:34:49 -0500

    loading from DB if index file erased

--- lib/app/hnswlib_index.ex
+++ lib/app/hnswlib_index.ex
@@ -31,46 +31,14 @@ defmodule App.HnswlibIndex do
         |> App.Repo.insert()
 
         Logger.info("New Index")
-
         HNSWLib.Index.new(_space = space, _dim = dim, _max_elements = max_elements)
 
       index_file ->
         Logger.info("Loading Index from DB")
         path = App.KnnIndex.get_index_path()
-
         File.write!(path, index_file.file)
-        |> dbg()
-
         HNSWLib.Index.load_index(space, dim, path)
-        |> dbg()
-
-        # |> :erlang.binary_to_term()
-        # |> then(fn content ->
-        #   dbg(content)
-        #   File.write!(content, path)
-        # end)
-        # |> dbg()
-
         HNSWLib.Index.load_index(space, dim, path)
     end
   end
-
-  # def load(space, dim, max_elements) do
-  #   Repo.get_by(HnswlibIndex, id: 1)
-  #   |> case do
-  #     nil ->
-  #       # App.KnnIndex.get_index_path()
-  #       # |> File.read!()
-  #       # |> :erlang.term_to_binary()
-  #       # |> then(fn t ->
-  #       #   App.Repo.insert(%App.HnswlibIndex{file: t})
-  #       # end)
-
-  #       Logger.info("New Index")
-  #       HNSWLib.Index.new(_space = space, _dim = dim, _max_elements = max_elements)
-
-  #     index_file ->
-  #       {:ok, :erlang.binary_to_term()}
-  #   end
-  # end
 end
