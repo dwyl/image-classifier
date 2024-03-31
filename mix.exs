00088464a76aa322d091198ab542cec1a34c19e1
LuchoTurtle <lucho14garfield@gmail.com>
2024-03-31 20:08:58 +0100

    fix: Maintain elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
